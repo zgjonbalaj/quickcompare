51762f410c6282fcc966ddc58a0565fbedd7e5c8
Zgjim Gjonbalaj <zgjim.gjonbalaj@agencylabs.com>
2018-02-13 16:01:03 -0500

    Added browserstack local

--- run.js
+++ run.js
@@ -11,26 +11,26 @@ fileSys.createDir(screenDir);
 
 // Prompt configuration
 var promptSchema = {
-	properties: {
-		devSite: {
-			description: 'Enter the URL of the development site',
-			type: 'string',
-			pattern: '^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$',
-			required: true
-		},
-		proSite: {
-			description: 'Enter the URL of the production site',
-			type: 'string',
-			pattern: '^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$',
-			required: true
-		}
-	}
+  properties: {
+    devSite: {
+      description: 'Enter the URL of the development site',
+      type: 'string',
+      pattern: '^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$',
+      required: true
+    },
+    proSite: {
+      description: 'Enter the URL of the production site',
+      type: 'string',
+      pattern: '^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$',
+      required: true
+    }
+  }
 };
 
 prompt.start();
 
 prompt.get(promptSchema, function(err, result) {
-	for(var site in promptSchema.properties) {
-		console.log(result[site]);
-	}
+  for(var site in promptSchema.properties) {
+    console.log(result[site]);
+  }
 });
